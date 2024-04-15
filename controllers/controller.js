ebdeef6b7ca4e0045e5a39bc33a43058ab5788fd
BCRAMO200 <BCRAMO200@CALEDONIAN.AC.UK>
2024-04-15 11:46:44 +0100

    set up routes and controllers

---
+++ controllers/controller.js
@@ -0,0 +1,37 @@
+
+exports.landing_page = function(req, res) {
+            res.render('home', {
+                'title': 'home',
+            })
+}
+exports.contact = function(req, res) {
+    res.render('contact', {
+    'title': 'Contact Us'
+    })
+}
+
+exports.catelogueP = function(req, res) {
+    res.render('catelogueP', {
+    'title': 'Perishable Catelogue'
+    })
+}
+exports.catelogueNP = function(req, res) {
+    res.render('catelogueNP', {
+    'title': 'Non-Perishable Catelogue'
+    })
+}
+exports.additem = function(req, res) {
+    res.render('additem', {
+    'title': 'Add item'
+    })
+}
+exports.about = function(req, res) {
+    res.render('about', {
+    'title': 'About Us'
+    })
+}
+exports.account = function(req, res) {
+    res.render('account', {
+    'title': 'My Account'
+    })
+}
---
+++ routes/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/controller.js');
+router.get("/", controller.landing_page);
+router.get('/contact', controller.contact);
+router.get('/catelogueP', controller.catelogueP);
+router.get('/catelogueNP', controller.catelogueNP);
+router.get('/additem', controller.additem);
+router.get('/about', controller.about);
+router.get('/account', controller.account);
+
+router.use(function(req, res) {
+    res.status(404);
+    res.type('text/plain');
+    res.send('404 Not found.');
+})
+router.use(function(err, req, res, next) {
+    res.status(500);
+    res.type('text/plain');
+    res.send('Internal Server Error.');
+})
+module.exports = router;
